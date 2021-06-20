149849083ffdc231051975f69d4757df3d4dd0a2
Aniruddha770 <patilanirudha38@gmail.com>
2021-06-20 10:27:35 +0530

    UC1 Added

---
+++ snakeLadder.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class snakeLadder {
+	public static void main(String[] args) {
+		int playerposition=0;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Player Name: ");
+		String playername = s.next();
+		
+				
+	}
+
+}
